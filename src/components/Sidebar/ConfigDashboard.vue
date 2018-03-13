06d21d481144f68f758300a4374a8357b251b9a2
Câmara <tiago.camara@cgi.com>
2018-03-13 18:25:03 +0000

    configure dashboard update

--- src/components/Sidebar/ConfigDashboard.vue
+++ src/components/Sidebar/ConfigDashboard.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="configure-separator">
     <h1>HEY!</h1>
-    <table class="grid-chooser">
-      <tr v-for="(n,index) in gridChooser.numRows" :key="index">
-        <td v-for="(n,index) in gridChooser.numCols" :key="index">
+    <table :id="gridChooser.tableId" class="grid-chooser">
+      <tr v-for="(x,index) in gridChooser.numRows" :key="index">
+        <td v-for="(y,index) in gridChooser.numCols" :key="index">
+          <div @mouseover="hoverFX(x,y)"
+               @click="selectGrid(x,y)">
+          </div>
+        </td>
+      </tr>
+    </table>
+    <table :id="gridLayout.tableId">
+      <tr v-for="(x,index) in gridLayout.numRows" :key="index">
+        <td v-for="(y,index) in gridLayout.numCols" :key="index">
         </td>
       </tr>
     </table>
@@ -11,7 +20,8 @@
 </template>
 
 <script>
-import tableMerger from './table-merger/table-merger.js';
+import jQuery from 'jquery'
+import tableMerger from './table-merger/table-merger.js'
 
 export default {
   name: 'configdashboard',
@@ -20,9 +30,39 @@ export default {
     return{
       gridChooser: 
       {
+        tableId: "gridChooserTable",
         numRows: 5,
         numCols: 5
+      },
+      gridLayout:
+      {
+        tableId: "gridConfigTable",
+        numRows: 0,
+        numCols: 0
       }
+    }
+  },
+  methods:
+  {
+    hoverFX: function(x,y)
+    {
+      var table = document.getElementById(this.gridChooser.tableId); //get table
+      jQuery(table).find("td").removeClass("selected"); //remove selected
+      var tr = table.children;
+
+      //update selected nodes
+      for(var i=0; i < x; i++)
+      {
+        var td = tr[i].children;
+        for(var j=0; j < y; j++)
+        {
+          td[j].classList.add("selected");
+        }
+      }
+    },
+    selectGrid: function(x,y)
+    {
+
     }
   }
 }
@@ -41,15 +81,22 @@ export default {
 table.grid-chooser
 {
   table-layout: fixed;
-  width: 85%;
+  width: 90%;
   height: 25%;
   cursor: pointer;
 }
 
-table.grid-chooser td
+table.grid-chooser td div
 {
-  padding: 2px;
-  border: 1px solid #ccc;
+  padding: 8px;
+  margin: 2px;
+  border: 1px solid $main-text-color;
+}
+
+table.grid-chooser td.selected div,
+table.grid-chooser td div:hover
+{
+  border-color: $thirdary-color;
 }
 
 </style>
